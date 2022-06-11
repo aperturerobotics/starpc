020a31e7fb9c30f97c2337c7d9cb29f95ad5521a
Christian Stewart <christian@paral.in>
2022-06-10 20:08:34 -0700

    e2e: commonize server->client stream checks
    Signed-off-by: Christian Stewart <christian@paral.in>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aperturerobotics/starpc/echo"
 	"github.com/aperturerobotics/starpc/srpc"
+	"github.com/pkg/errors"
 )
 
 // RunE2E runs an end to end test with a callback.
@@ -49,34 +50,44 @@ func TestE2E_Unary(t *testing.T) {
 	})
 }
 
+// CheckServerStream checks the server stream portion of the Echo test.
+func CheckServerStream(t *testing.T, out echo.SRPCEchoer_EchoServerStreamClient, req *echo.EchoMsg) error {
+	// expect to rx 5, then close
+	expectedRx := 5
+	totalExpected := expectedRx
+	for {
+		echoMsg, err := out.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		body := echoMsg.GetBody()
+		bodyTxt := req.GetBody()
+		if body != bodyTxt {
+			return errors.Errorf("expected %q got %q", bodyTxt, body)
+		}
+		t.Logf("server->client message %d/%d", totalExpected-expectedRx+1, totalExpected)
+		expectedRx--
+	}
+	if expectedRx < 0 {
+		return errors.Errorf("got %d more messages than expected", -1*expectedRx)
+	}
+	return nil
+}
+
 func TestE2E_ServerStream(t *testing.T) {
 	ctx := context.Background()
 	RunE2E(t, func(client echo.SRPCEchoerClient) error {
 		bodyTxt := "hello world"
-		out, err := client.EchoServerStream(ctx, &echo.EchoMsg{
+		req := &echo.EchoMsg{
 			Body: bodyTxt,
-		})
+		}
+		out, err := client.EchoServerStream(ctx, req)
 		if err != nil {
 			t.Fatal(err.Error())
 		}
-		// expect to rx 5, then close
-		expectedRx := 5
-		for {
-			echoMsg, err := out.Recv()
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				return err
-			}
-			body := echoMsg.GetBody()
-			if body != bodyTxt {
-				t.Fatalf("expected %q got %q", bodyTxt, body)
-			}
-		}
-		if expectedRx < 0 {
-			t.Fatalf("got %d more messages than expected", -1*expectedRx)
-		}
-		return nil
+		return CheckServerStream(t, out, req)
 	})
 }
