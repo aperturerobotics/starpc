c99dac875a3e4151134b226635245dfd99dc0adc
Christian Stewart <christian@paral.in>
2022-09-07 01:16:52 -0700

    feat(stream): add cast to duplex uint8array
    Signed-off-by: Christian Stewart <christian@paral.in>

--- srpc/stream.ts
+++ srpc/stream.ts
@@ -10,3 +10,10 @@ export type Stream = Duplex<Uint8ArrayList | Uint8Array, Uint8Array>
 
 // OpenStreamFunc is a function to start a new RPC by opening a Stream.
 export type OpenStreamFunc = () => Promise<Stream>
+
+// streamToUint8ArrayDuplex converts a Stream to a Duplex<Uint8Array>
+// This should be possible without the cast:
+// https://github.com/achingbrain/it-stream-types/issues/28
+export function streamToUint8ArrayDuplex(stream: Stream): Duplex<Uint8Array, Uint8Array> {
+  return stream as Duplex<Uint8Array> // eslint-disable-line
+}
