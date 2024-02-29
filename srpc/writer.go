603c18ed388d2d6ede93b65c20b8f7aee363e18b
Christian Stewart <christian@aperture.us>
2024-02-28 21:56:33 -0800

    feat: add new packet writer with close
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- srpc/writer.go
+++ srpc/writer.go
@@ -7,3 +7,27 @@ type PacketWriter interface {
 	// Close closes the writer.
 	Close() error
 }
+
+// packetWriterWithClose is a PacketWriter with a wrapped Close function.
+type packetWriterWithClose struct {
+	PacketWriter
+	closeFn func() error
+}
+
+// NewPacketWriterWithClose wraps a PacketWriter with a close function to call when Close is called.
+func NewPacketWriterWithClose(prw PacketWriter, close func() error) PacketWriter {
+	return &packetWriterWithClose{PacketWriter: prw, closeFn: close}
+}
+
+// Close closes the stream for reading and writing.
+func (s *packetWriterWithClose) Close() error {
+	err := s.PacketWriter.Close()
+	err2 := s.closeFn()
+	if err != nil {
+		return err
+	}
+	return err2
+}
+
+// _ is a type assertion
+var _ PacketWriter = (*packetWriterWithClose)(nil)
